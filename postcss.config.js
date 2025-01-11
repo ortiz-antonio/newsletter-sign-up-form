3c75912e707c68070916856e4e703cd7932e9a9e
Antonio Ortiz <ortiz.antonio@gmail.com>
2025-01-10 23:07:35 -0600

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    require("postcss-import-ext-glob"),
+    require("postcss-import"),
+    require("tailwindcss"),
+    ...(process.env.NODE_ENV === "production"
+      ? [require("cssnano")({ preset: "default" })]
+      : []),
+  ],
+};
