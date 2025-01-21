50ad9d46c7d4a770cd745b6aefa03afd754e461c
Antonio Ortiz <ortiz.antonio@gmail.com>
2025-01-20 18:34:17 -0600

    updated architecture to generate theme based on design tokens

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,25 @@
 import { defineConfig, presetUno, transformerDirectives } from 'unocss'
 
-import myTheme from './my-theme'
+import colorTokens from './src/tokens/colors.json'
+import fontSizeTokens from './src/tokens/font-sizes.json'
+import fontTokens from './src/tokens/fonts.json'
+import spacingTokens from './src/tokens/spaces.json'
 
 export default defineConfig({
-  presets: [presetUno({ preflight: false })],
+  presets: [
+    presetUno(),
+  ],
   transformers: [transformerDirectives()],
-  theme: myTheme,
+  extendTheme: (theme) => {
+    return {
+      ...theme,
+      colors: colorTokens.colors,
+      fontWeight: fontTokens.fontWeight,
+      lineHeight: fontTokens.lineHeight,
+      fontFamily: fontTokens.fontFamily,
+      fontSize: fontSizeTokens.fontSize,
+      spacing: spacingTokens.spacing,
+    }
+  },
+
 })
