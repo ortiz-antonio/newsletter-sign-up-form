fc8e55e043793bd1099632bd18eafdc02f4b8082
Antonio Ortiz <ortiz.antonio@gmail.com>
2025-02-04 14:24:33 -0600

    Updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,39 @@
-const success = document.getElementById("success");
-const form = document.getElementById("subscribe");
-const inputEmail = document.getElementById("email");
-const submitBtn = document.getElementById("submit");
+const success = document.getElementById('success');
+const form = document.getElementById('subscribe');
+const inputEmail = document.getElementById('email');
+const submitBtn = document.getElementById('submit');
 
-inputEmail?.addEventListener("input", () => {
-  inputEmail.setCustomValidity(""); // Clear any previous validation message
-  inputEmail.checkValidity(); // Re-check validity
+inputEmail?.addEventListener('input', () => {
+    inputEmail.setCustomValidity(''); // Clear any previous validation message
+    inputEmail.checkValidity(); // Re-check validity
 });
 
-form?.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const formData = new FormData(form);
-  const data = Object.fromEntries(formData.entries());
-  if (validateEmail(data.email)) {
-    submitBtn.setAttribute("aria-expanded", "true");
-    inputEmail.setAttribute("aria-invalid", "false");
+form?.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const formData = new FormData(form)
+    const data = Object.fromEntries(formData.entries())
+    if (validateEmail(data.email)) {
+        submitBtn.setAttribute('aria-expanded', 'true');
+        inputEmail.setAttribute('aria-invalid', 'false');
 
-    success.showModal();
-  } else {
-    inputEmail.focus();
-    inputEmail.setAttribute("aria-invalid", "true");
-    inputEmail.setCustomValidity("Invalid email format.");
-    form.reportValidity();
-  }
-});
+        success.showModal();
+    } else {
+        inputEmail.focus();
+        inputEmail.setAttribute('aria-invalid', 'true');
+        inputEmail.setCustomValidity("Invalid email format.");
+        form.reportValidity();
+    }
 
-const btnDismiss = document.querySelector("#dismiss");
+})
 
-btnDismiss.addEventListener("click", (event) => {
-  success.close();
-  submitBtn.setAttribute("aria-expanded", "false");
-});
+const btnDismiss = document.querySelector('#dismiss');
+
+btnDismiss.addEventListener('click', (event) => {
+    success.close()
+    submitBtn.setAttribute('aria-expanded', 'false')
+})
 
 function validateEmail(email) {
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailPattern.test(email);
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
 }
